930357566fe5b64ac56db752982a6b00ca572801
zach <wz.horton@gmail.com>
2020-08-24 16:47:43 -0700

    add browser/email install

---
+++ scripts/7_browser_email.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install Brave
+sudo apt install apt-transport-https curl -y
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+# Install Mailspring
+sudo snap install mailspring
+
+# Remove Bundled
+sudo apt purge firefox thunderbird -y
+
